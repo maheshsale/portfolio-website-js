49920f70c49f65c7f26d643ea3c684ddf2b66613
Mahesh Sale <maheshsale333@gmail.com>
2023-11-30 13:30:24 +0530

    Updated about me section

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ const experienceLink = document.getElementById('experience-link');
 const projectsLink = document.getElementById('projects-link');
 const contactLink = document.getElementById('contact-link');
 
+const contactInfo = document.getElementById('contact-info');
+
 aboutLink.addEventListener('click', function(){
       document.getElementById('about-me-heading').scrollIntoView({behavior: 'smooth'});
 });
@@ -21,5 +23,10 @@ projectsLink.addEventListener('click', function(){
 });
 
 contactLink.addEventListener('click', function(){
+      document.getElementById('contact-me-heading').scrollIntoView({behavior: 'smooth'});
+});
+
+
+contactInfo.addEventListener('click', function(){
       document.getElementById('contact-me-heading').scrollIntoView({behavior: 'smooth'});
 });
